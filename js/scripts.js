eb4e2101a3da8ee078caf5a01137a6ac1c27726e
BlaineR41 <blaineruttan@gmail.com>
2022-04-18 11:07:01 -0400

    working on Modal

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ const galleryContainer = document.getElementById('gallery');
 const searchBox = document.querySelector('.search-container');
 const bodyHtml = document.querySelector('body');
 let currentModalIndex;
-let found = true;  
+let found = true;
 
 
 /************** Get Employee Data ******************/
@@ -61,35 +61,77 @@ fetch(urlAPI)
 searchBox.insertAdjacentHTML('beforeend', searchField);
 
   /********************* Searching for Employee *******************************/
-  const searchBoxInput = document.getElementById('search-input');
-  //Gets search Input field
-  searchBoxInput.addEventListener('keyup', employeeSearch); //keyup event listener for search
+  const searchBoxInput = document.getElementById('search-input');    //Gets search input field
+searchBoxInput.addEventListener('keyup', employeeSearch);          // Keyup event listener for search
 
-  function employeeSearch(){
-    const input = searchBoxInput.value.toLowerCase(); //gets input and saves Input
-    const empCards = document.getElementByClassName('card'); //gets employees Cards
-    const empNames = document.getElementByClassName('card-name'); //Gets employee names
-    let employeeDisplayed = 0; //tracks number of employees found
+function employeeSearch() {
 
-    //when employee found, remove "notfound message"
-    if (found === false){
-      const noResultsFound = document.getElementById("no-results"); //get no result message
-      noResultsFound.remove();
+    const input = searchBoxInput.value.toLowerCase();   //Gets input and continuously saves to "input"
+    const empCards = document.getElementsByClassName('card');       //Gets employee cards
+    const empNames = document.getElementsByClassName('card-name');  //Gets eemployee names
+    let employeesDisplayed = 0;    // Tracks number of employees found
+
+    // If employee found, removes "Not found message"
+    if(found === false){
+        const noResultsFound = document.getElementById("no-results");   // Gets no results message
+        noResultsFound.remove();
     };
 
-    //loop for searching names
-    for (let i = 0; i < employees.length; i++){
-      if(empNames[i].textContent.toLowerCase().includes(input)){
-          empCards[i].style.display = '';
-          employeeDisplayed++;
-          found = true;
-      }else {
-        empCards[i].style.display ='none';
-      }
+    // Loop for searching names
+    for (let i= 0; i < employees.length; i++) {
+        if (empNames[i].textContent.toLowerCase().includes(input)) {
+            empCards[i].style.display = '';
+            employeesDisplayed++;
+            found = true;
+        } else {
+            empCards[i].style.display = 'none';
+        }
     }
-    //not found display message
-    if(employeeDisplayed == 0){
-      galleryContainer.insertAdjacentHTML('beforeend', "<h1 id = 'no-results'> oops, no one found by that name </h1>");
-      found = false;
+    // Not found message display
+    if(employeesDisplayed == 0) {
+        galleryContainer.insertAdjacentHTML('beforeend', "<h1 id = 'no-results'>Oops, their are no students by that name.</h1>");
+        found = false;
     }
-  }
+}
+
+/***************** Display Modal (student focus pop-up) ********************************/
+function displayModal(index) {
+
+    const {name, dob, phone, email, location :{city, street, state, postcode}, picture} = employees[index];
+    const newFormatPhone = phone.replace(/-/,' ');  // Format phone number
+    const birthday = formatDateOfBirth(dob);        // Format date of birth
+
+    const modalHTML = `
+        <div class="modal-container">
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src="${picture.large}" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">${name.first} ${name.last}</h3>
+                    <p class="modal-text">${email}</p>
+                    <p class="modal-text cap">${city}</p>
+                    <hr>
+                    <p class="modal-text">${newFormatPhone}</p>
+                    <p class="modal-text">${street.number} ${street.name}, ${city}, ${state} ${postcode}</p>
+                    <p class="modal-text">Birthday: ${birthday}</p>
+                </div>
+            </div>
+            <div class="modal-btn-container">
+                <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                <button type="button" id="modal-next" class="modal-next btn">Next</button>
+            </div>
+        </div>
+    `;
+
+    bodyHtml.insertAdjacentHTML('beforeend', modalHTML); //Put in Document
+    const modalClose = document.getElementById('modal-close-btn'); //close button
+    const modalButtons = document.querySelector('.modal-btn-container'); //prev and next buttons
+    const nextEmployee = document.getElementById('modal-next'); //next button
+    const prevEmployee = document.getElementById('modal-prev'); //previous button
+
+//close modal
+modalClose.addEventListener('click', e =>{
+  document.body.removeChild(document.body.lastElementChild);
+});
+
+}
